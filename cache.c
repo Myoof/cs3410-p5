b527b9120d80760b38418210af66b6cfd76d45a5
Aaron Song <ams799@cornell.edu>
2024-04-29 01:50:11 -0400

    Complete Tasks 1, 2, 3
    - Implemented make_cache by fixing parts of cache.c
    - Implemented functions get_cache_tag(), get_cache_index(), and get_cache_block_addr().

--- cache.c
+++ cache.c
@@ -6,43 +6,53 @@
 #include "cache.h"
 #include "print_helpers.h"
 
-cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t protocol, bool lru_on_invalidate_f){
+cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t protocol, bool lru_on_invalidate_f)
+{
   cache_t *cache = malloc(sizeof(cache_t));
   cache->stats = make_cache_stats();
-  
-  cache->capacity = capacity;      // in Bytes
-  cache->block_size = block_size;  // in Bytes
-  cache->assoc = assoc;            // 1, 2, 3... etc.
+
+  cache->capacity = capacity;     // in Bytes
+  cache->block_size = block_size; // in Bytes
+  cache->assoc = assoc;           // 1, 2, 3... etc.
 
   // FIX THIS CODE!
   // first, correctly set these 5 variables. THEY ARE ALL WRONG
   // note: you may find math.h's log2 function useful
-  cache->n_cache_line = 1;
-  cache->n_set = 1;
-  cache->n_offset_bit = 1;
-  cache->n_index_bit = 1;
-  cache->n_tag_bit = 1;
+  cache->n_cache_line = capacity / block_size;
+  cache->n_set = capacity / (block_size * assoc);
+  cache->n_offset_bit = log2(block_size);
+  cache->n_index_bit = log2(capacity / (block_size * assoc));
+  cache->n_tag_bit = 32 - cache->n_offset_bit - cache->n_index_bit;
 
   // next create the cache lines and the array of LRU bits
   // - malloc an array with n_rows
   // - for each element in the array, malloc another array with n_col
   // FIX THIS CODE!
 
-  cache->lines = NULL;
-  cache->lru_way = NULL;
+  cache->lines = malloc(cache->n_set * sizeof(cache_line_t));
+  for (int i = 0; i < cache->n_set; i++)
+  {
+    cache->lines[i] = malloc(cache->assoc * sizeof(cache_line_t));
+  }
+
+  cache->lru_way = malloc(cache->assoc * sizeof(cache_line_t));
 
   // initializes cache tags to 0, dirty bits to false,
   // state to INVALID, and LRU bits to 0
   // FIX THIS CODE!
-  for (int i = 0; i < 1; i++) {
-    for (int j = 0; j < 1; j++) {
-      // body goes here
+  for (int i = 0; i < cache->n_set; i++)
+  {
+    for (int j = 0; j < cache->assoc; j++)
+    {
+      cache->lines[i][j].tag = 0;
+      cache->lines[i][j].dirty_f = false;
+      cache->lines[i][j].state = INVALID;
     }
   }
 
   cache->protocol = protocol;
   cache->lru_on_invalidate_f = lru_on_invalidate_f;
-  
+
   return cache;
 }
 
@@ -50,11 +60,12 @@ cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t pro
  *
  * Example: a cache with 4 bits each in tag, index, offset
  * in binary -- get_cache_tag(0b111101010001) returns 0b1111
- * in decimal -- get_cache_tag(3921) returns 15 
+ * in decimal -- get_cache_tag(3921) returns 15
  */
-unsigned long get_cache_tag(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_tag(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
-  return 0;
+  return addr >> (cache->n_index_bit + cache->n_offset_bit);
 }
 
 /* Given a configured cache, returns the index portion of the given address.
@@ -63,9 +74,13 @@ unsigned long get_cache_tag(cache_t *cache, unsigned long addr) {
  * in binary -- get_cache_index(0b111101010001) returns 0b0101
  * in decimal -- get_cache_index(3921) returns 5
  */
-unsigned long get_cache_index(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_index(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
-  return 0;
+  // Contains the address without the offset
+  int addrNoOffset = addr >> cache->n_offset_bit;
+  int mask = (1 << cache->n_index_bit) - 1;
+  return mask && addrNoOffset;
 }
 
 /* Given a configured cache, returns the given address with the offset bits zeroed out.
@@ -74,23 +89,25 @@ unsigned long get_cache_index(cache_t *cache, unsigned long addr) {
  * in binary -- get_cache_block_addr(0b111101010001) returns 0b111101010000
  * in decimal -- get_cache_block_addr(3921) returns 3920
  */
-unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
-  return 0;
+  unsigned long ones = ~0;
+  ones = ones << cache->n_offset_bit;
+  return ones & addr;
 }
 
-
 /* this method takes a cache, an address, and an action
- * it proceses the cache access. functionality in no particular order: 
+ * it proceses the cache access. functionality in no particular order:
  *   - look up the address in the cache, determine if hit or miss
  *   - update the LRU_way, cacheTags, state, dirty flags if necessary
  *   - update the cache statistics (call update_stats)
  * return true if there was a hit, false if there was a miss
  * Use the "get" helper functions above. They make your life easier.
  */
-bool access_cache(cache_t *cache, unsigned long addr, enum action_t action) {
+bool access_cache(cache_t *cache, unsigned long addr, enum action_t action)
+{
   // FIX THIS CODE!
 
-
-  return true;  // cache hit should return true
+  return true; // cache hit should return true
 }
